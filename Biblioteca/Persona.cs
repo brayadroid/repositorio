a1bda0b18a16cf5737839a01158e0220d15674a8
novacane <espinoza.sample@gmail.com>
2015-05-26 18:09:43 -0400

    y

---
+++ Biblioteca/Persona.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Biblioteca
+{
+    abstract class Persona
+    {
+        private string nombre;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        private int edad;
+
+        public int Edad
+        {
+            get { return edad; }
+            set { edad = value; }
+        }
+        private string email;
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+
+    }
+}
---
+++ CentroDeEventos/Cl_empleado.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CentroDeEventos
+{
+    public class Cl_empleado
+    {
+        private string _nombre;
+
+        public string Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+        private SEXO _sexo;
+
+        public SEXO Sexo
+        {
+            get { return _sexo; }
+            set { _sexo = value; }
+        }
+        private int _edad;
+
+        public int Edad
+        {
+            get { return _edad; }
+            set { _edad = value; }
+        }
+        private int _fechanacimiento;
+
+        public int Fechanacimiento
+        {
+            get { return _fechanacimiento; }
+            set { _fechanacimiento = value; }
+        }
+        private string _labor;
+
+        public string Labor
+        {
+            get { return _labor; }
+            set { _labor = value; }
+        }
+        private string _horarioDisponible;
+
+        public string HorarioDisponible
+        {
+            get { return _horarioDisponible; }
+            set { _horarioDisponible = value; }
+        }
+    }
+}
